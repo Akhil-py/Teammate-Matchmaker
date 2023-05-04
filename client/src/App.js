64a05412e1d4f420796ddf082a5fbf3da94e0a5d
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2023-05-04 16:51:49 -0700

    Better Basic Navbar
    Moved folders under Navbar and removed previous react Logo thingy

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import logo from './logo.svg';
 
 import Navbar from "./Navbar";
-import AboutUs from "./AboutUs";
-import Contact from './Contact';
-import MemberLogin from './Member Login';
-import Team from './Team';
+import Home from "./Navbar/Home";
+import AboutUs from "./Navbar/AboutUs";
+import Contact from './Navbar/Contact';
+import MemberLogin from './Navbar/Member Login';
+import Team from './Navbar/Team';
+import Profile from './Navbar/Profile';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -17,28 +18,14 @@ function App() {
 
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<MemberLogin />} />
             <Route path="/team" element={<Team />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
-      </BrowserRouter>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+        </BrowserRouter>
       </div>
     </main>
   );
