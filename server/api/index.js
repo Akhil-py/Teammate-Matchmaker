88b55fb5ca698658d40a137202e258f9996e86ee
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2023-05-18 16:53:15 -0700

    update

---
+++ server/api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../models/users');
+//const Valorant = require('../models/valorant');
+//const League = require('../models/league');
+
+const router = express.Router();
+
+// CRUD operations for User data
+router.get('/users', async (req, res) => {
+    const users = await User.find().exec();
+    res.status(200).json({ users });
+});
+
+router.post('/users', async (req, res) => {
+    const { user } = req.body;
+    const {username, email, password, discord, college} = user;
+    if (!username || !email || !password || !discord || !college) {
+        res.status(400).json({error: 'Invalid Input!'});
+    }
+    else {
+        const  newUser = await User.create(user);
+        res.status(200).json({newUser});
+    }
+});
+
+module.exports = router;
