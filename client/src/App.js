1a3c38c4996bf9d351caefcfc2d542e62b741b0a
Calvin Nguyen <can005@ucsd.edu>
2023-05-18 20:56:08 -0700

    added animations

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,11 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import AboutUs from "./Navbar/AboutUs";
 import Contact from './Navbar/Contact';
-import MemberLogin from './Member Login';
+import MemberLogin from './Member_Login';
 import Team from './Navbar/Team';
 import Profile from './Navbar/Profile';
 import Register from './Register';
+import MemberLanding from './Member_Landing';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -26,6 +27,7 @@ function App() {
             <Route path="/team" element={<Team />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/memberlanding" element={<MemberLanding />} />
           </Routes>
         </BrowserRouter>
       </div>
--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./styles.css";
 import jett from "../Images/jett.png";
 import sage from "../Images/sage.png";
 import temo from "../Images/temo.png";
@@ -8,19 +8,13 @@ import lol from "../Images/lol.png";
 import yumi from "../Images/yumi.png";
 
 function Home() {
-    return(
+    return (
     <div>
         <div class="container-1">
             <div class="text-box">
-                <div class="text-content">
-                    🎮 Find Gamer Friends at UC San Diego
-                </div>
-                <div class="text-content">
-                    Find you the Perfect Match🤝
-                </div>
-                <div class="text-content">
-                    No Matter What Rank or Role You Are
-                </div>
+                <h1>🎮 Find Gamer Friends at UC San Diego</h1>
+                <h1>Find you the Perfect Match🤝</h1>
+                <h1>No Matter What Rank or Role You Are</h1>
             </div>
             <div class="img-container">
                 <img src={jett} alt="bruh"></img>
@@ -33,13 +27,21 @@ function Home() {
                 <img src={yumi} alt="bruh"></img>
             </div>
             <div class="text-box-2">
-                <div class="text-content-2">
-                    See all the Games We Support!
-                </div>
+                <h1>See all the Games We Support!</h1>
+                <div class="text-img-box">
+                    <div class="text-img-box-row">
+                        <h1>Valorant</h1>
+                        <img src={val} alt="valorant"></img>
+                    </div>
+                    <div class="text-img-box-row">
+                        <h1>League of Legends</h1>
+                        <img src={lol} alt="league"></img>
+                    </div>
+                </div>       
             </div>
         </div>
     </div>
-    );
-}
+    )
+};
 
 export default Home;
