3ee9ff86d16b9768b11bc90903f12ae929bbad2d
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2023-05-25 13:29:45 -0700

    Updated Navbar
    Bring Calvin's fancy stuff to Kane's Navbar

--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./styles.css";
 import jett from "../Images/jett.png";
 import sage from "../Images/sage.png";
@@ -7,14 +7,21 @@ import val from "../Images/val.png";
 import lol from "../Images/lol.png";
 import yumi from "../Images/yumi.png";
 
+// make sure to "npm install aos"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 function Home() {
+    useEffect(() => {
+        AOS.init({duration: 2000});
+    }, []); 
     return (
     <div>
         <div class="container-1">
-            <div class="text-box">
-                <h1>🎮 Find Gamer Friends at UC San Diego</h1>
-                <h1>Find you the Perfect Match🤝</h1>
-                <h1>No Matter What Rank or Role You Are</h1>
+            <div class="text-box" >
+                <h1 data-aos="fade-down">🎮 Find Gamer Friends at UC San Diego</h1>
+                <h1 data-aos="fade-right">Find you the Perfect Match🤝</h1>
+                <h1 data-aos="fade-up">No Matter What Rank or Role You Are</h1>
             </div>
             <div class="img-container">
                 <img src={jett} alt="bruh"></img>
@@ -23,17 +30,17 @@ function Home() {
         </div>
         <div class="container-2">
             <div class="img-container-2">
-                <img src={temo} alt="bruh"></img>
-                <img src={yumi} alt="bruh"></img>
+                <img data-aos="fade-right" src={temo} alt="bruh"></img>
+                <img data-aos="fade-right" src={yumi} alt="bruh"></img>
             </div>
             <div class="text-box-2">
-                <h1>See all the Games We Support!</h1>
+                <h1 data-aos="fade-down">See all the Games We Support!</h1>
                 <div class="text-img-box">
-                    <div class="text-img-box-row">
+                    <div class="text-img-box-row" data-aos="fade-left">
                         <h1>Valorant</h1>
                         <img src={val} alt="valorant"></img>
                     </div>
-                    <div class="text-img-box-row">
+                    <div class="text-img-box-row" data-aos="fade-right">
                         <h1>League of Legends</h1>
                         <img src={lol} alt="league"></img>
                     </div>
--- client/src/Navbar/index.js
+++ client/src/Navbar/index.js
@@ -1,28 +1,52 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
-import logo from "./logo.png"
+import logo from "../Images/logo.png";
+import logo_only from "../Images/logo_only.png";
 
 const loginOrProfile = (isLoggedIn) => {
   console.log("isLoggedIn ", isLoggedIn);
   if(isLoggedIn.values){
     return <a class="navlinks" href="/profile">Profile</a>
   }
-  return <a class="navlinks" href="/login">Member Login</a>;
+  return <a class="navlinks" href="/login">
+    <h2 class="underline-hover-effect">
+      Login
+    </h2>
+  </a>;
 }
 
 function Navbar(values) {
 
     return(
       <div className="navbar">
-        <a className="navlogo" href="/">
-            <img style={{width: 220, height: 80}} src={logo} alt="Logo">
-            </img>
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+          <link href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap" rel="stylesheet"></link>
+        </head>
+        <a href="/">
+          <div class="logo-container">
+            <img class="navlogo" src={logo_only} alt="Logo"></img>
+            <img class="navname" src={logo} alt="gamed"></img>
+          </div>
         </a>
-        <nav className="navlinks">
-          <a className="navlinks" href="/about">About us</a>
-          <a className="navlinks" href="/team">Team</a>
-          <a className="navlinks" href="/contact">Contact</a>
+        <nav className="nav-container">
+          <a className="navlinks" href="/about">
+            <h2 class="underline-hover-effect">
+              About us
+            </h2>
+          </a>
+          <a className="navlinks" href="/team">
+            <h2 class="underline-hover-effect">
+              Team
+            </h2>
+          </a>
+          <a className="navlinks" href="/contact">
+            <h2 class="underline-hover-effect">
+              Contact
+            </h2>
+          </a>
           {loginOrProfile(values)}
         </nav>
       </div>
