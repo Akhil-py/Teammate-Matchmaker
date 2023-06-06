3c7e9870636d37f269fdb8d6ff5697f5075fc7f4
SithuSoe04 <63730799+SithuSoe04@users.noreply.github.com>
2023-06-06 16:27:29 -0700

    updated AboutUs page

--- client/src/AboutUs/index.js
+++ client/src/AboutUs/index.js
@@ -1,41 +1,51 @@
 import React from "react";
 import "./index.css";
 
-function AboutUS() {
+function AboutUs() {
   return (
     <div className="aboutus-container">
       <div className="description">
-        <p className="text"> 
-          Introducing GAMED - The Ultimate Platform for Connecting Gamers of
-          Similar Rank and Region. GAMED is an innovative website designed to
-          bring gamers together based on their rank and region, revolutionizing
-          the way players connect and engage in online gaming communities.
-          Whether you're a casual gamer looking for like-minded individuals or
-          a competitive player seeking challenging opponents, GAMED is your
-          go-to destination. With GAMED, finding gamers who share your skill
-          level and reside in your region has never been easier. Say goodbye to
-          endless searches and frustrating matchmaking experiences. Our
-          intelligent algorithm swiftly matches you with fellow gamers who are
-          not only at a similar rank but also located in your geographical
-          area, enhancing your gaming experience by fostering local connections
-          and reducing latency. See the slide below for more information on how we developed this 
-          website!
+        <p className="text">
+          GAMED is an innovative website designed to bring gamers together
+          based on their rank and region, revolutionizing the way players
+          connect and engage in online gaming communities. Whether you're a
+          casual gamer looking for like-minded individuals or a competitive
+          player seeking challenging opponents, GAMED is your go-to
+          destination.
         </p>
       </div>
-      <div className="slides">
-        <iframe
-          src="https://docs.google.com/presentation/d/e/2PACX-1vS_118sGyzJ3cmgbaSJby5C7ux8q-uVwe1UaNU0EMLVLU0cN4TPGRgksslP8dqvZg73EmNp0sUZ_JBY/embed?start=true&loop=true&delayms=10000"
-          frameBorder="0"
-          width="960"
-          height="569"
-          allowFullScreen={true}
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-          title="Google Slides"
-        ></iframe>
+      <div className="features">
+        <div className="feature">
+          <img src={require("./Images/gamerconnect.png")} alt="Gamers" className="feature-image" />
+          <h2>Connect with Gamers</h2>
+          <p>
+            Discover and connect with gamers who share your skill level and
+            passion for gaming. Forge new friendships and join gaming
+            communities tailored to your interests.
+          </p>
+        </div>
+        <div className="feature">
+          <img src={require("./Images/rank.jpeg")} alt="Rank" className="feature-image" />
+          <h2>Rank-based Matching</h2>
+          <p>
+            Our intelligent algorithm swiftly matches you with fellow gamers who
+            are at a similar rank, ensuring balanced and competitive gameplay
+            experiences.
+          </p>
+        </div>
+        <div className="feature">
+          <img src={require("./Images/region.png")} alt="Region" className="feature-image" />
+          <h2>Regional Connections</h2>
+          <p>
+            Find gamers located in your region to reduce latency and foster
+            local connections. Enhance your gaming experience with low-latency
+            multiplayer sessions.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
-export default AboutUS;
+export default AboutUs;
+
